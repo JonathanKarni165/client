3f3f93d62b20a124c9e92fd78bb3f74e7c1c2627
JonatanKarni165 <yonatankarni165@gmail.com>
2022-09-22 14:15:43 +0300

    new sign in panel

--- src/InputBar.js
+++ src/InputBar.js
@@ -1,28 +1,37 @@
 import React from "react";
 import "./InputBar.css";
 import { useState } from "react";
+import { setUserAgent } from "react-device-detect";
 
 function InputBar({ callback, className, submit }) {
   const [text, setText] = useState("");
 
+
   return (
-    <div className={className}>
-      <input
-        className="inbar"
-        id="input"
-        type="text"
-        onChange={(input) => setText(input.target.value)}
-        value={text}
-      />
-      <button
-        className="sndbtn"
-        onClick={() => {
-          if (text !== "") callback(text);
-          setText("");
-        }}
-      >
-        {submit}
-      </button>
+    <div className="placeholder">
+
+      <div className="big-bar-div">
+        <input
+          className="inbar"
+          id="input"
+          type="text"
+          onChange={(input) => setText(input.target.value)}
+          value={text}
+        />
+
+      </div>
+
+      <div className="small-bar-div">
+        <button
+          className="sndbtn"
+          onClick={() => {
+            if (text !== "") callback(text);
+            setText("");
+          }}
+        >
+          {submit}
+        </button>
+      </div>
     </div>
   );
 }
